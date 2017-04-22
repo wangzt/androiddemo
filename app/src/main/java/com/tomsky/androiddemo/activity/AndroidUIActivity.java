717a2c7156dc3394be74baf100bc1c6b7b5f205c
wangzhitao <wangzhitao@huajiao.tv>
2017-04-22 16:34:43 +0800

    added ClipView

--- app/src/main/java/com/tomsky/androiddemo/activity/AndroidUIActivity.java
+++ app/src/main/java/com/tomsky/androiddemo/activity/AndroidUIActivity.java
@@ -1,9 +1,15 @@
 package com.tomsky.androiddemo.activity;
 
+import android.animation.FloatArrayEvaluator;
+import android.animation.IntArrayEvaluator;
+import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
 
 import com.tomsky.androiddemo.R;
+import com.tomsky.androiddemo.view.ClipView;
 
 /**
  * Created by j-wangzhitao on 17-1-12.
@@ -11,9 +17,41 @@ import com.tomsky.androiddemo.R;
 
 public class AndroidUIActivity extends Activity {
 
+    private ClipView mClipView;
+
+    private int left = 0;
+    private int right = 0;
+
+    private boolean start = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_ui);
+        mClipView = (ClipView) findViewById(R.id.my_clip_view);
+
+        findViewById(R.id.btn_start_anim).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int start[] = {0, 10000};
+                int center[] = {0, 0};
+                int center2[] = {0, 0};
+                int end[] = {10000, 0};
+
+                ValueAnimator valueAnimator = ValueAnimator.ofObject(new IntArrayEvaluator(), start, center, center2, end);
+                valueAnimator.setDuration(2000);
+                valueAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+                valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        int []value = (int[]) animation.getAnimatedValue();
+
+                        mClipView.setClipPadding(value[0], value[1]);
+                    }
+                });
+                valueAnimator.start();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/tomsky/androiddemo/view/ClipView.java
@@ -0,0 +1,72 @@
+package com.tomsky.androiddemo.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by j-wangzhitao on 17-4-22.
+ */
+
+public class ClipView extends ImageView {
+
+    private Drawable mImageDrawable;
+
+    private boolean mCropToPadding = false;
+
+    private int mPaddingLeft = 0;
+    private int mPaddingRight = 0;
+
+    public ClipView(Context context) {
+        this(context, null);
+    }
+
+    public ClipView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ClipView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        if (mImageDrawable == null) {
+            mImageDrawable = getDrawable();
+        }
+
+        if (mImageDrawable != null) {
+            if (mPaddingLeft == 0 && mPaddingRight == 0) {
+                mImageDrawable.draw(canvas);
+                return;
+            }
+            int saveCount = canvas.getSaveCount();
+            canvas.save();
+
+            if (mCropToPadding) {
+                final int scrollX = getScrollX();
+                final int scrollY = getScrollY();
+                canvas.clipRect(scrollX + mPaddingLeft, getScrollY(),
+                        scrollX + getRight() - getLeft() - mPaddingRight,
+                        scrollY + getBottom() - getTop());
+            }
+
+            canvas.translate(mPaddingLeft, 0);
+
+            mImageDrawable.draw(canvas);
+            canvas.restoreToCount(saveCount);
+        }
+    }
+
+    public void setClipPadding(int paddingLeft, int paddingRight) {
+        int width = getWidth();
+        this.mPaddingLeft = paddingLeft*width/10000;
+        this.mPaddingRight = paddingRight*width/10000;
+        mCropToPadding = true;
+        invalidate();
+    }
+}
